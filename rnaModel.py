2c60e02bea9b380903d11537d9a2dc1beaba43be
cazcullimore <cazcullimore@gmail.com>
2025-05-14 20:08:47 -0600

    addedTriangleAttention

--- rnaModel.py
+++ rnaModel.py
@@ -1,8 +1,8 @@
 import torch
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from time import time
+from math import sqrt
 
 class RNAModel(nn.Module):
     def __init__(self, inputDim, hiddenDim, outputDim,device):
@@ -13,7 +13,10 @@ class RNAModel(nn.Module):
         self.tBlock1 = nn.TransformerEncoder(encoder_layer, 2)
         self.expandChannels = nn.Linear(1,expandDim)
         
-        self.triangleMul = TriangleMultiplication(expandDim)
+        self.triangleMul = TriangleMultiplicationOut(expandDim)
+        self.triangleAtten = TriangleSelfAttention(expandDim)
+        self.triangleMul2 = TriangleMultiplicationOut(expandDim)
+        self.triangleMul3 = TriangleMultiplicationOut(expandDim)
         self.lastLayer = nn.Linear(expandDim,outputDim)
         self.outputDim = outputDim
         self.hiddenDim = hiddenDim
@@ -29,7 +32,10 @@ class RNAModel(nn.Module):
         output = torch.matmul(output, output.transpose(1,2)).unsqueeze(-1) # change to distogram shape: batchSize, seqLen, seqLen, 1
         output = self.expandChannels(output)
         output = self.triangleMul(output)
-
+        output =  self.triangleAtten(output)
+        print("self atten")
+        output = self.triangleMul2(output)
+        output = self.triangleMul3(output)
         # outputShape = output.shape[:2] + (self.outputDim,)
         # output = output.view(-1,self.hiddenDim)
  
@@ -39,7 +45,7 @@ class RNAModel(nn.Module):
         return output
     
 
-class TriangleMultiplication(nn.Module):
+class TriangleMultiplicationOut(nn.Module):
     def __init__(self,input_channels, internalChannels=128):
         super().__init__()
         self.layerNorm = nn.LayerNorm(input_channels)
@@ -60,6 +66,55 @@ class TriangleMultiplication(nn.Module):
         
 
         return output
+    
+class TriangleMultiplicationIn(nn.Module):
+    def __init__(self,input_channels, internalChannels=128):
+        super().__init__()
+        self.layerNorm = nn.LayerNorm(input_channels)
+        self.firstAttenLin = nn.Linear(input_channels, internalChannels)
+        self.secondAttenLin = nn.Linear(input_channels, internalChannels)
+        self.thirdAttenLin = nn.Linear(input_channels, internalChannels)
+        self.outNorm = nn.LayerNorm(internalChannels)
+        self.outLin = nn.Linear(internalChannels, input_channels)
+
+    def forward(self, input:torch.tensor):
+        normalized = self.layerNorm(input)
+        a = F.sigmoid(self.firstAttenLin(normalized)) * self.secondAttenLin(normalized)
+        b = F.sigmoid(self.firstAttenLin(normalized)) * self.secondAttenLin(normalized)
+        g = F.sigmoid(self.thirdAttenLin(normalized))
+        out = torch.einsum('bkic,bkjc->bijc', a, b)        
+
+        output = g * self.outLin(self.outNorm(out))
+        
+
+        return output
+    
+class TriangleSelfAttention(nn.Module):
+    def __init__(self,input_channels, internalChannels=128):
+        super().__init__()
+        self.internalChannels = internalChannels
+        self.layerNorm = nn.LayerNorm(input_channels)
+        self.q_lin = nn.Linear(input_channels, internalChannels,bias=False) # TODO: implement multiple heads
+        self.k_lin = nn.Linear(input_channels, internalChannels,bias=False)
+        self.v_lin = nn.Linear(input_channels, internalChannels,bias=False)
+        self.b_lin = nn.Linear(input_channels, 1,bias=False) 
+        self.g_lin = nn.Linear(input_channels, internalChannels)
+        self.outLin = nn.Linear(internalChannels, input_channels)
+
+    def forward(self, input:torch.tensor):
+        normalized = self.layerNorm(input)
+        q = self.q_lin(normalized)
+        k = self.k_lin(normalized)
+        v = self.v_lin(normalized)
+        b = self.b_lin(normalized)
+        g = F.sigmoid(self.g_lin(normalized))
+        a =  F.softmax(1/sqrt(self.internalChannels) * torch.einsum('bijc,bikc->bijk',q,k) + b, dim=-2) # softmax over k dim
+        out = g * torch.einsum('bijk,bikc->bijc',a,v)   
+
+        out =  self.outLin(out) # TODO: concat multiheads when added
+        
+
+        return out
     
 
 
